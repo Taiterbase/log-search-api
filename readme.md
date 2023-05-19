dcc7d1d3d7ad3cb4c926d05306f8df4d40b8a596
Taite Nazifi <taitenazifi@gmail.com>
2023-05-18 19:27:47 -0700

    init

---
+++ readme.md
@@ -0,0 +1,24 @@
+## Benchmarks
+
+Benchmarks made possible by log file supporters:
+Shilin He, Jieming Zhu, Pinjia He, Michael R. Lyu. Loghub: A Large Collection of System Log Datasets towards Automated Log Analytics. Arxiv, 2020.
+
+### 1. HDFS.1 (Hadoop Distributed File System) 1.47GB
+
+[src/main.rs:85] &req.filename = "install.log"
+[src/main.rs:85] elapsed_time = 572.818167ms
+
+### 2. HDFS.2 (Hadoop Distributed File System) 16.06GB
+
+[src/main.rs:85] &req.filename = "install.log"
+[src/main.rs:85] elapsed_time = 572.818167ms
+
+### 2. Hadoop (Hadoop madreduce job log) 48.61MB
+
+### 3. Spark 2.75GB
+
+### 5. BGL (Blue Gene/L) 708.76MB
+
+### 6. HPC (High Performance Computing) 32.00MB
+
+### 7. Windows 26.09GB
---
+++ src/main.rs
@@ -0,0 +1,107 @@
+use std::fs::File;
+use std::io::{self, Read, Seek, SeekFrom};
+use std::time::Instant;
+
+use actix_files as fs;
+use actix_web::{web, App, HttpResponse, HttpServer, Result};
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+struct LogsRequest {
+    filename: String,
+    keyword: Option<String>,
+    last: Option<usize>,
+}
+
+//const CHUNK_SIZE: u64 = 4096; // 4 KiB, can tweak as needed
+const CHUNK_SIZE: u64 = 4096;
+
+async fn log_lines(req: web::Query<LogsRequest>) -> Result<HttpResponse, io::Error> {
+    let path = format!("/var/log/{}", req.filename);
+
+    if path.contains("..") {
+        // simple and unsophisticated way to prevent path traversal attacks
+        return Ok(HttpResponse::BadRequest().body("Bad Request"));
+    }
+
+    let start_time = Instant::now();
+
+    let mut file: File = File::open(&path)?;
+    let file_size_mb =
+            // truncate to 2 decimal places
+            f64::trunc(file.metadata()?.len() as f64 / (1024.0 * 1024.0) * 100.0) / 100.0;
+    let mut buf: Vec<u8> = Vec::new();
+    let mut lines: Vec<String> = Vec::new();
+    let mut position: u64 = file.metadata()?.len();
+
+    while position > 0 {
+        let chunk_size: u64 = if position >= CHUNK_SIZE {
+            CHUNK_SIZE
+        } else {
+            position
+        };
+
+        position = position.saturating_sub(chunk_size);
+
+        file.seek(SeekFrom::Start(position))?;
+        buf.resize(chunk_size as usize, 0);
+        file.read_exact(&mut buf)?;
+
+        let mut chunk_lines: Vec<String> = buf
+            // doesn't take into account other line endings
+            .split(|b: &u8| *b == b'\n')
+            .map(|s: &[u8]| String::from_utf8_lossy(s).into_owned())
+            .collect();
+
+        if !lines.is_empty() {
+            // The first line may be incomplete, append it to the last line of the next chunk
+            let first_line = chunk_lines.pop().unwrap();
+            let last_line: &mut String = lines.last_mut().unwrap();
+            last_line.insert_str(0, &first_line);
+        }
+
+        // newest lines to oldest lines
+        chunk_lines.reverse();
+        lines.append(&mut chunk_lines);
+
+        // fast path to return the last N lines
+        if let Some(last) = req.last {
+            if lines.len() >= last {
+                break;
+            }
+        }
+    }
+
+    lines = lines
+        .into_iter()
+        .filter(|line: &String| {
+            req.keyword
+                .as_ref()
+                .map_or(true, |keyword: &String| line.contains(keyword))
+        })
+        .collect();
+
+    if let Some(last) = req.last {
+        lines.truncate(last);
+    }
+
+    let elapsed_time = start_time.elapsed();
+    println!(
+        "{:?} ({:?}mb):\t{:?}",
+        &req.filename, file_size_mb, elapsed_time
+    );
+
+    Ok(HttpResponse::Ok().json(lines))
+}
+
+#[actix_web::main]
+async fn main() -> io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .route("/logs", web::get().to(log_lines))
+            .service(fs::Files::new("/", "/var/log").show_files_listing())
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
+}
