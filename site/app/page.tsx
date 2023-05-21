3d33cada0cc478c8c2e8be0622d30488e7e6748f
Taite Nazifi <taitenazifi@gmail.com>
2023-05-20 17:12:25 -0700

    accessibility labels and some cleanup

--- site/app/page.tsx
+++ site/app/page.tsx
@@ -1,11 +1,12 @@
 'use client';
 
 import Image from 'next/image'
+import clsx from 'clsx';
 import { useState } from 'react';
 
 const Header = () => {
   return (
-    <div className="sticky top-0 flex flex-row justify-between px-4 z-10 w-full items-center font-mono text-sm backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800 dark:from-inherit">
+    <section about="Header" className="sticky top-0 flex flex-row justify-between px-4 z-10 w-full items-center font-mono text-sm backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800 dark:from-inherit">
       <Image
         src={'/cribl.svg'}
         alt="Cribl Logo"
@@ -23,9 +24,26 @@ const Header = () => {
           rel="noopener noreferrer"
         >Taite</a></code>
       </p>
-    </div>
+    </section>
   )
 }
+
+function LogDisplay({ logs, serverIndex }: { logs: string[], serverIndex?: number }) {
+  return (
+    <div className={clsx({
+      "flex-1 max-h-[70vh] p-2 rounded": serverIndex === undefined,
+      "flex-1 max-h-[25vh] overflow-auto bg-neutral-800 p-2 rounded": serverIndex !== undefined,
+    })}>
+      {serverIndex !== undefined && <h3 className="font-medium text-lg">Server&nbsp;{serverIndex}</h3>}
+      {
+        logs.map((log, index) => (
+          <pre key={`${serverIndex}+${index}`} className="text-left text-xs font-mono whitespace-pre-wrap">{log}</pre>
+        ))
+      }
+    </div>
+  );
+}
+
 
 export default function Home() {
   const [logs, setLogs] = useState<string[]>([]);
@@ -64,13 +82,24 @@ export default function Home() {
   }
 
   return (
-    <main className="flex w-full h-full overflow-auto min-h-screen flex-col items-center">
+    <main className="flex flex-col w-full h-full overflow-auto min-h-screen items-center">
       <Header />
       <form onSubmit={(e) => fetchLogs(e)} className="mx-auto grid md:grid-cols-5 gap-y-4 md:gap-x-4 md:gap-y-0 my-4 p-4 items-center h-full w-11/12 md:w-9/12 mt-8 rounded-xl shadow-md bg-neutral-100 px-8" >
-        <input className="p-2 w-full rounded bg-white border border-neutral-700" type="text" placeholder="filename" value={filename} onChange={(e) => setFilename(e.target.value)} />
-        <input className="p-2 w-full rounded bg-white border border-neutral-700" type="number" placeholder="last" value={last} onChange={(e) => setLast(parseInt(e.target.value))} />
-        <input className="p-2 w-full rounded bg-white border border-neutral-700" type="text" placeholder="keyword" value={keyword} onChange={(e) => setKeyword(e.target.value)} />
+        <label htmlFor="filename" className="sr-only">Filename</label>
+        <input className="p-2 w-full rounded bg-white border border-neutral-700" id="filename" type="text" aria-label="filename" placeholder="filename" value={filename} onChange={(e) => setFilename(e.target.value)} />
+
+        <label htmlFor="last" className="sr-only">Last</label>
+        <input className="p-2 w-full rounded bg-white border border-neutral-700" id="last" type="number" aria-label="last-n" placeholder="last" value={last} onChange={(e) => setLast(parseInt(e.target.value))} />
+
+        <label htmlFor="keyword" className="sr-only">Keyword</label>
+        <input className="p-2 w-full rounded bg-white border border-neutral-700" id="keyword" type="text" aria-label="keyword" placeholder="keyword" value={keyword} onChange={(e) => setKeyword(e.target.value)} />
+
         <div className="flex flex-row justify-center space-x-4 h-6 items-center">
+          <div className="text-sm leading-6">
+            <label htmlFor="comments" className="font-medium text-gray-900">
+              Multi Server
+            </label>
+          </div>
           <input
             id="comments"
             aria-describedby="comments-description"
@@ -80,42 +109,18 @@ export default function Home() {
             checked={multi}
             onChange={(e) => setMulti(e.target.checked)}
           />
-          <div className="text-sm leading-6">
-            <label htmlFor="comments" className="font-medium text-gray-900">
-              Multi Server
-            </label>
-          </div>
         </div>
         <button className="bg-neutral-400 h-12 rounded-xl hover:bg-neutral-300" type="submit">Submit</button>
       </form>
 
-      <div className="flex-grow grid gap-y-2 mx-auto mb-8 md:mt-8 rounded-xl text-neutral-300 shadow-2xl shadow-fuchsia-900 bg-neutral-700 w-11/12 p-3">
+      <div className="flex-grow overflow-auto grid gap-y-2 mx-auto mb-8 md:mt-8 rounded-xl text-neutral-300 shadow-2xl shadow-fuchsia-900 bg-neutral-700 w-11/12 p-3">
         {
-          multi && multiLogs && multiLogs.map((l, i) => {
-            return (
-              <div key={i} className="max-h-[30rem] overflow-auto bg-neutral-800 p-2 rounded">
-                <h2 className="font-medium text-lg">Server&nbsp;{i}</h2>
-                {
-                  l.map((log, index) => {
-                    return (
-                      <pre key={`${i}+${index}`} className="text-left text-xs font-mono whitespace-pre-wrap">{log}</pre>
-                    )
-                  })
-                }
-              </div>
-            )
-          })
+          multi && multiLogs && multiLogs.map((logs, serverIndex) => (
+            <LogDisplay key={serverIndex} logs={logs} serverIndex={serverIndex} />
+          ))
         }
         {
-          !multi && logs && <div className="bg-neutral-800 p-2 rounded flex flex-col">
-            {
-              !multi && logs && logs.map((l, index) => {
-                return (
-                  <pre key={index} className="text-left text-xs font-mono whitespace-pre-wrap">{l}</pre>
-                )
-              })
-            }
-          </div>
+          !multi && logs && <LogDisplay logs={logs} />
         }
       </div>
     </main >
